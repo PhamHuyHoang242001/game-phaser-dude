ce47799fd4d7dda1b5e04565a796310d36a4b76e
huyhoang242001 <huyhoang.pham@secomus.com>
2024-12-31 12:17:19 +0700

    'fix:webpack'

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  entry: './src/ts/App.ts',
+  entry: "./src/ts/App.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },  
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
-    extensions: [ '.ts', '.tsx', '.js' ]
+    extensions: [".ts", ".tsx", ".js"],
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "app.js",
+    path: path.resolve(__dirname, "public"),
   },
-  mode: 'development'
+  mode: "development",
 };
