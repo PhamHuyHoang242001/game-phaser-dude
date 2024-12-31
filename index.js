5ec5127984060ea23b26de71e02b0e60865a6f85
huyhoang242001 <huyhoang.pham@secomus.com>
2024-12-31 12:20:41 +0700

    'fix:public'

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var express = require('express')
-var path = require( 'path' )
-var app = express()
-
+var express = require("express");
+var path = require("path");
+var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use('/dist', express.static(path.join(__dirname, '/dist')));
-app.use('/assets', express.static(path.join(__dirname, '/assets')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use("/assets", express.static(path.join(__dirname, "/assets")));
 
-app.get('/', function(req,res){
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/homepage.html");
-})
+});
 
-app.listen(port, function(){
-	console.log(__dirname);
-	console.log('Simple Server Running on port 8080')
-})
+app.listen(port, function () {
+  console.log(__dirname);
+  console.log("Simple Server Running on port 8080");
+});
